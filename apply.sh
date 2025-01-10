8625e399c1f8ad3da88244a570da925857202172
luna :3 <ellipticobj@gmail.com>
2025-01-10 23:54:13 +0800

     commit

---
+++ apply.sh
@@ -0,0 +1,17 @@
+cp -r ./.oh-my-zsh ~/
+cp -r ./.themes ~/
+cp -r ./.icons ~/
+cp -r ./.vscode ~/
+cp -r ./.wakatime ~/
+cp -r ./.config/neofetch ~/.config/
+cp -r ./.config/micro ~/.config/
+cp -r ./.config/kitty ~/.config/
+cp -r ./.config/gtk-3.0 ~/.config/
+cp -r ./.config/gtk-4.0 ~/.config/
+
+cp ./.shell.pre-oh-my-zsh ~/
+cp ./.p10k.zsh ~/
+cp ./.wakatime.cfg ~/
+cp ./.zshrc ~/
+cp ./.zshrc.pre-oh-my-zsh ~/
+cp ./.zshrc.zni ~/
